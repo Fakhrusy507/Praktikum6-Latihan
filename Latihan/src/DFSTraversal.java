4b650831d376a81771f9f3389a72079003cecca6
Fakhrusy507 <127365456+Fakhrusy507@users.noreply.github.com>
2023-06-08 09:47:25 +0700

    Add files via upload

---
+++ Latihan/src/DFSTraversal.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+
+public class DFSTraversal {
+    final LinkedList<Integer>[] adj;
+    final boolean[] visited;
+
+    DFSTraversal(int V) {
+        adj = new LinkedList[V];
+        visited = new boolean[V];
+
+        for (int i = 0; i < V; i++)
+            adj[i] = new LinkedList<>();
+    }
+
+    void insertEdge(int src, int dest) {
+        adj[src].add(dest);
+    }
+
+    void DFS(int vertex) {
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+
+        for (int n : adj[vertex]) {
+            if (!visited[n])
+                DFS(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        DFSTraversal graph = new DFSTraversal(8);
+
+        graph.insertEdge(0, 1);
+        graph.insertEdge(0, 2);
+        graph.insertEdge(0, 3);
+        graph.insertEdge(1, 3);
+        graph.insertEdge(2, 4);
+        graph.insertEdge(3, 5);
+        graph.insertEdge(3, 6);
+        graph.insertEdge(4, 7);
+        graph.insertEdge(4, 5);
+        graph.insertEdge(5, 2);
+
+        System.out.println("Depth First Traversal for the graph is:");
+        graph.DFS(0);
+    }
+}
